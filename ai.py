e55b2046b2e7b1e2b731b98955e16e7a3e513e45
Aldo Monteiro <aldo.fsmonteiro@gmail.com>
2018-08-01 20:06:49 -0300

    target network

--- ai.py
+++ ai.py
@@ -1,13 +1,14 @@
+import time
 import tensorflow as tf
 import numpy as np
-import time
 
 class NeuralNet:
-    def __init__(self, nb_inputs, nb_hidden, nb_outputs, learning_rate, logdir=None):
+    def __init__(self, session, nb_inputs, nb_hidden, nb_outputs, learning_rate, logdir=None):
         self.logdir = logdir
         self.iterations = 0
         self.nb_outputs = nb_outputs
-        self.session = tf.Session()
+        self.parameters = []
+        self.session = session
         self.x = tf.placeholder(tf.float32, shape=(None, nb_inputs), name='input')
         prev_layer = self.x
         prev_layer_size = nb_inputs
@@ -17,8 +18,10 @@ class NeuralNet:
             h = tf.nn.relu(tf.matmul(prev_layer, W) + b, name='hidden'+str(i))
             prev_layer = h
             prev_layer_size = nb_hidden[i]
+            self.parameters.extend([W, b])
         W = tf.Variable(0.1*np.random.randn(prev_layer_size, nb_outputs), dtype=tf.float32, name='W'+str(len(nb_hidden)))
         b = tf.Variable(0.1*np.random.randn(nb_outputs), dtype=tf.float32, name='b'+str(len(nb_hidden)))
+        self.parameters.extend([W, b])
         self.y = tf.add(tf.matmul(prev_layer, W), b, name='output')
         
         self.y_target = tf.placeholder(tf.float32, shape=(None, nb_outputs), name='target_output')
@@ -30,12 +33,13 @@ class NeuralNet:
         self.softmax_temperature = tf.placeholder(tf.float32, name='softmax_temperature')
         self.probs = tf.nn.softmax(tf.gather(self.y/self.softmax_temperature, self.softmax_indexes, axis=1, name='probabilities'))
         
-        self.variable_initializer = tf.global_variables_initializer()
-        self.session.run(self.variable_initializer)
-
         self.loss_summary = tf.summary.scalar('loss', self.loss)
         if logdir:
             self.writer = tf.summary.FileWriter(logdir, self.session.graph)
+
+    def assignParameters(self, new_parameters):
+        for i in range(len(self.parameters)):
+            self.session.run(self.parameters[i].assign(new_parameters[i]))
 
     def learn(self, x, y_target):
         summary, _ = self.session.run(
@@ -75,13 +79,19 @@ class Dqn:
         self.reward_window_length = 100
         self.reward_window = []
         self.softmax_temperature = softmax_temperature
-        self.batch_size = 7000
+        self.target_net_update_steps = 150
+        self.batch_size = 1000
         self.memory_capacity = 10000
         self.memory = ExperienceReplay(self.memory_capacity)
         self.last_state = None
         self.last_action = None
-        self.brain = NeuralNet(9, nb_hidden, 9, learning_rate, logdir='/tmp/tensorboard/ia_velha/'+str(time.time()))
-        # self.brain = NeuralNet(9, nb_hidden, 9, learning_rate)
+        self.session = tf.Session()
+        self.target_network = NeuralNet(self.session, 9, nb_hidden, 9, learning_rate)
+        logdir='/tmp/tensorboard/ia_velha/batch_size={}, steps_to_refresh={}, {} - {}'.format(self.batch_size, self.target_net_update_steps, [reward_decay, nb_hidden, learning_rate, softmax_temperature], time.ctime())
+        self.q_network = NeuralNet(self.session, 9, nb_hidden, 9, learning_rate, logdir=logdir)
+        # self.q_network = NeuralNet(9, nb_hidden, 9, learning_rate)
+        self.session.run(tf.global_variables_initializer())
+        self.training_steps = 0
         
     def encode_board(self, board):
         map_dict = {'X':-1,'':0,'O':1}
@@ -91,7 +101,7 @@ class Dqn:
         state = self.encode_board(state)
         available_actions = [i for i in range(len(state)) if not state[i]]
         if len(available_actions) > 0:
-            probs = self.brain.predict_probs(
+            probs = self.q_network.predict_probs(
                 [state],
                 softmax_temperature=self.softmax_temperature,
                 softmax_indexes=available_actions
@@ -114,15 +124,16 @@ class Dqn:
         print(self.average_score(), len(self.memory))
         self.reward_window.append(reward)
         self.reward_window = self.reward_window[-self.reward_window_length:]
+        
         action = self.choose_action(new_state)
         new_state = self.encode_board(new_state)
         if self.last_state:
             self.memory.append(self.last_state, new_state, self.last_action, reward)
         if len(self.memory) > 0:
             last_state_batch, new_state_batch, action_batch, reward_batch = self.memory.sample(self.batch_size)
-            new_state_value = np.max(self.brain.predict(new_state_batch), axis=1)
+            new_state_value = np.max(self.target_network.predict(new_state_batch), axis=1)
             target_q = reward_batch + self.reward_decay*new_state_value
-            current_q = self.brain.predict(last_state_batch)
+            current_q = self.q_network.predict(last_state_batch)
             
             target_batch = []
             for i in range(len(target_q)):
@@ -130,7 +141,9 @@ class Dqn:
                 row[action_batch[i]] = target_q[i]
                 target_batch.append(row)
             
-            self.brain.learn(last_state_batch, target_batch)
+            self.q_network.learn(last_state_batch, target_batch)
+            if self.training_steps % self.target_net_update_steps == 0:
+                self.target_network.assignParameters(self.q_network.parameters)
 
         self.last_action = action
         self.last_state = new_state
